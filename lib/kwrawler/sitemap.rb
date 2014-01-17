01298d3b2e59e05f9e0a81db3cc72a66baf1dc2c
Keith Woody <keith.woody@gmail.com>
2014-01-17 12:25:52 -0800

    first pass at specs and implementation of traverse_site
    * initialize site hash
    * add convenience classes for pages and links
    * process the html from the initial URI to extract assets and links

--- lib/kwrawler/sitemap.rb
+++ lib/kwrawler/sitemap.rb
@@ -1,36 +1,81 @@
 require 'open-uri'
+require 'nokogiri'
 
 class Sitemap
   URI_FAILURE = "failed to read uri"
 
-  @site_hash = { pages: [],
-                 links: [],
-                 static_assets: []
-  }
   attr_accessor :site_hash
+
+  class Page < Struct.new(:uri, :links, :properties); end
+  #status can be :new, :processed, :unreachable
+  class Link < Struct.new(:href, :status); end
+
+  def initialize
+    @site_hash = { pages: [],
+                   links: {},
+                   assets: { imgs: [],
+                             scripts: [],
+                             stylesheets: [] }
+                 }
+  end
 
   def from_uri( uri )
     contents = retrieve( uri )
-    unless contents.eq?( URI_FAILURE )
-      traverse_site( contents )
+    unless contents.eql?( URI_FAILURE )
+      traverse_site( uri, contents )
       render_sitemap
     end
   end
 
   def render_sitemap
-    "sitemap"
+    site_hash.inspect
   end
 
   def retrieve( uri )
     contents = ""
-    # begin
+    begin
       contents = open( uri ).read
-    # rescue #errors 404, 500, etc.
-    #   return URI_FAILURE
-    # end
+    rescue #errors 404, 500, etc.
+      return URI_FAILURE
+    end
     contents
   end
 
-  def traverse_site( html )
+  def traverse_site( uri, html )
+    page_hash = { assets: { imgs: [],
+                            scripts: [],
+                            stylesheets: [] },
+                  links: {}
+                 }
+    html_doc = Nokogiri::HTML( html )
+    # get all scripts, stylesheets and images
+    srcs = html_doc.css('script').map { |s| src = s.attributes['src']; src.value if src }.compact
+    srcs.each do |src|
+      page_hash[:assets][:scripts] << src unless page_hash[:assets][:scripts].include?( src )
+    end
+    site_hash[:assets][:scripts] |= page_hash[:assets][:scripts]
+
+    hrefs = html_doc.css('link[rel="stylesheet"]').map { |l| href = l.attributes['href']; href.value if href }.compact
+    hrefs.each do |href|
+      page_hash[:assets][:stylesheets] << href unless page_hash[:assets][:stylesheets].include?( href )
+    end
+    site_hash[:assets][:stylesheets] |= page_hash[:assets][:stylesheets]
+
+    imgs = html_doc.css('img').map { |i| img = i.attributes['src']; img.value if img  }.compact
+    imgs.each do |img|
+      page_hash[:assets][:imgs] << img unless page_hash[:assets][:imgs].include?( img )
+    end
+    site_hash[:assets][:imgs] |= page_hash[:assets][:imgs]
+
+    # get all internal links on the page
+    links = html_doc.css('a').map { |l| link = l.attributes['href']; link.value if link && (link.value !~ %r{^http:.*})  }.compact
+    links.each do |href|
+      link = Link.new( href, :new )
+      page_hash[:links][href] = link unless page_hash[:links].keys.include?( link )
+    end
+    site_hash[:links].merge!( page_hash[:links] )
+
+    site_hash[:pages] << Page.new(uri, :success, page_hash)
+    # TODO: process links
   end
 end
--- spec/models/kwrawler/sitemap_spec.rb
+++ spec/models/kwrawler/sitemap_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 require 'kwrawler/sitemap'
 
-describe Sitemap, focus: true do
+describe Sitemap do
   it { should respond_to( :from_uri ) }
   it { should respond_to( :render_sitemap ) }
   it { should respond_to( :retrieve ) }
@@ -9,10 +9,17 @@ describe Sitemap, focus: true do
 
   describe "#from_uri" do
     it "should require a uri" do
-      expect{ subject.from_uri }.to raise_error( ArgumentError )
+      expect{ subject.from_uri }.to raise_error
+      expect{ subject.from_uri('http://www.google.com') }.not_to raise_error
     end
   end
   describe "#render_sitemap" do
+    let(:sitemap) { subject.render_sitemap }
+    it "should output the site hash" do
+      expect( sitemap ).to match(/pages/)
+      expect( sitemap ).to match(/assets/)
+      expect( sitemap ).to match(/links/)
+    end
   end
   describe "#retrieve" do
     it "should return the contents of a URI" do
@@ -20,5 +27,27 @@ describe Sitemap, focus: true do
     end
   end
   describe "#traverse_site" do
+    subject!{ Sitemap.new } #to clear out the google retrieve
+    it "should add a page for the initial URI to the site hash" do
+      expect{ subject.traverse_site( :any_uri, '<html></html>' ) }.to change{ subject.site_hash[:pages].size }.from(0).to(1)
+    end
+    it "should add unique js sources to the assets collection" do
+      expect{ subject.traverse_site( :any_uri, '<html><head><script src="local.js"></script></head></html>' ) }.to change{ subject.site_hash[:assets][:scripts].size }.from(0).to(1)
+      expect( subject.site_hash.inspect ).to match(%r|local.js|)
+    end
+    it "should add unique stylesheets to the assets collection" do
+      expect{ subject.traverse_site( :any_uri, '<html><head><link rel="stylesheet" href="local.css"/></head></html>' ) }.to change{ subject.site_hash[:assets][:stylesheets].size }.from(0).to(1)
+      expect( subject.site_hash.inspect ).to match(%r|local.css|)
+    end
+    it "should add unique images to the assets collection" do
+      expect{ subject.traverse_site( :any_uri, '<html><body><img src="local.png"/><img src="local.png"/></body></html>' ) }.to change{ subject.site_hash[:assets][:imgs].size }.from(0).to(1)
+      expect( subject.site_hash.inspect ).to match(%r|local.png|)
+    end
+    it "should add internal links to the site hash" do
+      expect{ subject.traverse_site( :any_uri, '<html><body><a href="/internal">internal</a><a href="http://external.com/about">external</a></body></html>' ) }.to change{ subject.site_hash[:links].size }.from(0).to(1)
+      expect( subject.site_hash.inspect ).to match(%r|/internal|)
+      expect( subject.site_hash.inspect ).not_to match(%r|/external|)
+    end
+    it "should process each internal link like the root URI"
   end
 end
